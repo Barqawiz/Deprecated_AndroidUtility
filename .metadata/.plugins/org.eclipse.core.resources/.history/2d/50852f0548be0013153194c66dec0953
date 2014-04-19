8109ae68e0566826194eaa7b3a8594ef979a77f5
Ahmad Barqawi <Barqawi.88@outlook.com>
2014-04-19 15:04:42 +0300

    Edit Utility Class
    Add more methods to the general utility class “Utility” and organize
    the methods inside it based on type

--- .metadata/.plugins/org.eclipse.core.resources/.history/2d/50852f0548be0013153194c66dec0953
+++
@@ -1,6 +0,0 @@
-import android.app.Activity;
-
-
-public class MainActivity extends Activity {
-
-}
--- Android Utilities/src/com/iaraby/utility/Utility.java
+++ Android Utilities/src/com/iaraby/utility/Utility.java
@@ -21,6 +21,9 @@
  */
 package com.iaraby.utility;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 import com.iarbay.utility.R;
@@ -34,6 +37,9 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.telephony.TelephonyManager;
@@ -43,8 +49,14 @@ import android.widget.Toast;
 /**
  * General utilities that collect common methods used in most apps to save time
  * on developers
+ * 
+ * The methods divided into sections and for every section that have a lot of methods will 
+ * be moved into another class
+ * (Section: System, Files UI, Social, Operations)
  */
 public class Utility {
+
+	/* System */
 
 	/**
 	 * Check if Internet available even WIFI network or mobile data network
@@ -68,96 +80,7 @@ public class Utility {
 				haveConnectedMobile = true;
 		}
 		return haveConnectedWifi || haveConnectedMobile;
-	} // check internet connection
-
-	/**
-	 * check if screen orientation is portrait
-	 * 
-	 * @param screenOrientation
-	 * @return true if orientation portrait, false otherwise
-	 */
-	public static boolean isPortrait(int screenOrientation) {
-		if (screenOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
-			return true;
-		} else {
-			return false;
-		} // check orientation
-	} // method: check if portrait
-
-	/**
-	 * check if screen orientation is landscape
-	 * 
-	 * @param screenOrientation
-	 * @return true if orientation landscape, false otherwise
-	 */
-	public static boolean isLandscape(int screenOrientation) {
-		if (screenOrientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
-			return true;
-		} else {
-			return false;
-		} // check orientation
-	} // method: check if portrait
-
-	/**
-	 * show short toast message for the user
-	 * 
-	 * @param Context
-	 * @param String
-	 *            message
-	 */
-	public static void showToastMessage(Context context, String msg) {
-		if (msg == null) msg = "";
-
-		Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
-	} // method: show message
-
-	/**
-	 * check if the received String is numbers only
-	 * 
-	 * @param String
-	 *            value
-	 * @return true is the String is numbers only, false otherwise
-	 */
-	public static boolean isNumber(String value) {
-		try {
-			Integer.parseInt(value);
-		} catch (NumberFormatException ex) {
-			return false;
-		}
-		return true;
-	} // method: check if string number or not
-
-	/**
-	 * Call share intent to share data with other apps like Faebook, Twitter,
-	 * etc If you don't want to share link send empty String "" or null, don't
-	 * send null in message or subject field
-	 * 
-	 * @param String
-	 *            subject
-	 * @param String
-	 *            message
-	 * @param String
-	 *            link
-	 * @param Activity
-	 *            activity
-	 */
-	public static void share(String subject, String message, String link,
-			Activity activity) {
-
-		if (link == null || link.length() == 0) {
-			link = "";
-		} else {
-			link = "\n" + link;
-		}
-
-		Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-
-		sharingIntent.setType("text/plain");
-		sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-		sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, message
-				+ link);
-		activity.startActivity(Intent.createChooser(sharingIntent, "Share"));
-	}
+	} // check Internet connection
 
 	/**
 	 * Check if the app is visible on foreground to make sure it is not in sleep
@@ -183,31 +106,6 @@ public class Utility {
 			} // loop on all active processes
 		} // make sure app process list not null
 		return false;
-	}
-
-	/**
-	 * Copy new array based on received range from start to end if end > start
-	 * will return the original array without coping
-	 * 
-	 * @param String
-	 *            [] original array
-	 * @param int start coping from
-	 * @param int end coping
-	 * @return new array from start to end range
-	 */
-	public static String[] copyOfRange(String[] original, int start, int end) {
-
-		if (start < 0 || end > original.length || start > end)
-			return original;
-
-		String[] newArray = new String[end - start];
-
-		int index = 0;
-		for (int i = start; i < end; i++) {
-			newArray[index++] = original[i];
-		}
-
-		return newArray;
 	}
 
 	/**
@@ -240,27 +138,205 @@ public class Utility {
 		return opName;
 	}
 
+	/*Files*/
 	/**
-	 * Create loading dialog in center of screen, without text and prevent clicks
+	 * This method return the name of files and folders in assets 
 	 * 
-	 * to get the view call: findViewById(com.iarbay.utility.R.id.loading_dialog);
+	 * @param String folder name, if you want to list the root send null
 	 * 
-	 * @return ProgressDialog after make it visible, make sure to save it in variable 
-	 * to dismiss it when needed
+	 * @return Array of string for items names
+	 * @throws IOException 
+	 */
+	public static String[] listAssetsFiles(Context context, String folder) throws IOException {
+		if (folder == null)
+			folder = "";
+		String[] fileNames = context.getAssets().list(folder);
+		return fileNames;
+		
+	}
+	
+	/**
+	 * Read image from assets
+	 * @param Context
+	 * @param @param String folder name, if you want the root send null or empty string
+	 * @param String image name with extension e.g.image.png
+	 * @return Bitmap 
+	 * @throws IOException throw exception if the image not found or error while reading
+	 */
+	public static Bitmap getImageFromAssets(Context context, String folder, String filename)
+			throws IOException {
+		if (folder == null) folder = "";
+		if (folder.trim().length() > 0 && !folder.endsWith("/"))
+			folder = folder + "/";
+		
+		AssetManager manager = context.getAssets();
+		InputStream open = manager.open(folder + filename);
+
+		Bitmap bitmap = BitmapFactory.decodeStream(open);
+
+		open.close();
+
+		return bitmap;
+	} // read image form assets based on received name
+
+	/* UI */
+	/**
+	 * show short toast message for the user
+	 * 
+	 * @param Context
+	 * @param String
+	 *            message
+	 */
+	public static void showToastMessage(Context context, String msg) {
+		if (msg == null)
+			msg = "";
+
+		Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+	} // method: show message
+
+	/**
+	 * Create loading dialog in center of screen, without text and prevent
+	 * clicks
+	 * 
+	 * to get the view call:
+	 * findViewById(com.iarbay.utility.R.id.loading_dialog);
+	 * 
+	 * @return ProgressDialog after make it visible, make sure to save it in
+	 *         variable to dismiss it when needed
 	 */
 	public static ProgressDialog createLoadingDialog(Context context) {
 		ProgressDialog dialog = new ProgressDialog(context);
-		
+
 		try {
 			dialog.show();
-		} catch (BadTokenException e) {}
+		} catch (BadTokenException e) {
+		}
 		dialog.setCancelable(false);
 		dialog.setContentView(R.layout.progressdialog);
 		dialog.setOnCancelListener(new OnCancelListener() {
-			public void onCancel(DialogInterface dialog) {}
+			public void onCancel(DialogInterface dialog) {
+			}
 		});
-		
+
 		return dialog;
+	}
+
+	/**
+	 * check if screen orientation is portrait
+	 * 
+	 * @param screenOrientation
+	 * @return true if orientation portrait, false otherwise
+	 */
+	public static boolean isPortrait(int screenOrientation) {
+		if (screenOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+			return true;
+		} else {
+			return false;
+		} // check orientation
+	} // method: check if portrait
+
+	/**
+	 * check if screen orientation is landscape
+	 * 
+	 * @param screenOrientation
+	 * @return true if orientation landscape, false otherwise
+	 */
+	public static boolean isLandscape(int screenOrientation) {
+		if (screenOrientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+			return true;
+		} else {
+			return false;
+		} // check orientation
+	} // method: check if portrait
+
+	/* Social */
+	/**
+	 * Call share intent to share data with other apps like Faebook, Twitter,
+	 * etc If you don't want to share link send empty String "" or null, don't
+	 * send null in message or subject field
+	 * 
+	 * @param String
+	 *            subject
+	 * @param String
+	 *            message
+	 * @param String
+	 *            link
+	 * @param Activity
+	 *            activity
+	 */
+	public static void share(String subject, String message, String link,
+			Activity activity) {
+
+		if (link == null || link.length() == 0) {
+			link = "";
+		} else {
+			link = "\n" + link;
+		}
+
+		Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+
+		sharingIntent.setType("text/plain");
+		sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+		sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, message
+				+ link);
+		activity.startActivity(Intent.createChooser(sharingIntent, "Share"));
+	}
+
+	/* Operations */
+	/**
+	 * check if the received String is numbers only
+	 * 
+	 * @param String
+	 *            value
+	 * @return true is the String is numbers only, false otherwise
+	 */
+	public static boolean isNumber(String value) {
+		try {
+			Integer.parseInt(value);
+		} catch (NumberFormatException ex) {
+			return false;
+		}
+		return true;
+	} // method: check if string number or not
+
+	/**
+	 * Copy new array based on received range from start to end if end > start
+	 * will return the original array without coping
+	 * 
+	 * @param String
+	 *            [] original array
+	 * @param int start coping from
+	 * @param int end coping
+	 * @return new array from start to end range
+	 */
+	public static String[] copyOfRange(String[] original, int start, int end) {
+
+		if (start < 0 || end > original.length || start > end)
+			return original;
+
+		String[] newArray = new String[end - start];
+
+		int index = 0;
+		for (int i = start; i < end; i++) {
+			newArray[index++] = original[i];
+		}
+
+		return newArray;
+	}
+	
+	/**
+	 * copy from InputStream to  OutputStream
+	 * @param InputStream in
+	 * @param OutputStream out
+	 * @throws IOException
+	 */
+	public static void copyStream(InputStream in, OutputStream out)
+			throws IOException {
+		byte[] buffer = new byte[1024];
+		int read;
+		while ((read = in.read(buffer)) != -1) {
+			out.write(buffer, 0, read);
+		}
 	}
 
 } // class: Utility
